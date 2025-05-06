Fix bug field computation

meta_surf.py
@@ -138,7 +138,7 @@ class transmit_array:
         dp = np.empty(self.nb_cell)
         theta_out = np.empty(self.nb_cell)
         phi_out = np.empty(self.nb_cell)
-        rad_field = 0
+        rad_field = np.complex128(0.)
         
         idx = 0
         for x in self.x:
@@ -148,11 +148,15 @@ class transmit_array:
                 theta_out[idx] = np.acos(z_rad / dp[idx])
                 phi_out[idx] = np.acos((y_rad - y)/np.sqrt(np.square(x_rad - x) \
                                + np.square(y_rad - y)))
-                
-                rad_field = rad_field + \
-                    self.unit_cell.radiated_field_from_sig(
-                        self.output_sig[idx], wavelgth, dp[idx], 
+
+                rad_field_ucell = self.unit_cell.radiated_field_from_sig(
+                        self.output_sig[idx], wavelgth, dp[idx],
                         theta_out[idx], phi_out[idx])
+                # print([x, y, self.output_sig[idx], rad_field_ucell])
+                rad_field = rad_field + rad_field_ucell
+
+                idx = idx + 1
+
                     
         return rad_field
                 

test_meta_surf.py
@@ -8,15 +8,15 @@ unit_cell_side_lgth = 0.03
 dist_src = 0.5
 wavelgth = 0.06
 power = 1
-n_cells_x = 2
-n_cells_y = 1
+n_cells_x = 20
+n_cells_y = 20
 
 ucell = simple_unit_cell(unit_cell_side_lgth)
 horn = simplified_horn_source(5)
 
 ta = transmit_array(n_cells_x, n_cells_y, ucell, horn, dist_src)
-# ta.set_random_phase_shift()
-ta.set_specific_phase_shift()
+ta.set_random_phase_shift()
+# ta.set_specific_phase_shift()
 
 # plt.figure()
 # plt.imshow(np.abs(ta.output_sigs()))
@@ -81,7 +81,7 @@ n_y = 50
 n_z = 50
 x_rad = np.linspace(0, ta_width, n_x) - ta_width/2
 y_rad = np.linspace(0, ta_width, n_y) - ta_width/2
-z_rad = np.linspace(0, wavelgth, n_y)
+z_rad = np.linspace(0, ta_width, n_y)
 
 rad_field_yz = np.empty(n_x*n_y, dtype=np.complex128)
 
@@ -94,9 +94,10 @@ for z in z_rad:
 rad_field_yz = rad_field_yz.reshape(n_y, n_z)
         
 plt.figure()
-# plt.imshow(np.abs(rad_field))
-plt.imshow(np.real(rad_field_yz))
-# plt.imshow(np.abs(np.real(rad_field)))
+plt.imshow(np.abs(rad_field_yz))
+# plt.imshow(np.real(rad_field_yz))
+# plt.imshow(np.abs(np.real(rad_field_yz)))
+plt.colorbar()
 plt.show()
 
 rad_field_xz = np.empty(n_x*n_y, dtype=np.complex128)
@@ -110,5 +111,7 @@ for z in z_rad:
 rad_field_xz = rad_field_xz.reshape(n_x, n_z)
         
 plt.figure()
-plt.imshow(np.real(rad_field_xz))
+plt.imshow(np.abs(rad_field_xz))
+# plt.imshow(np.real(rad_field_xz))
+plt.colorbar()
 plt.show()