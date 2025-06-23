d3e7297fbec1cfe1a03bf42bc1ea9d3148c80c09
Rémi Blandin <rblandin@ietr.univ-rennes1.fr>
2025-06-23 16:48:30 +0200

    Try different optimization strategies

--- test_transmission_optimization.py
+++ test_transmission_optimization.py
@@ -6,10 +6,17 @@ import matplotlib.pyplot as plt
 #-----------------------------------------------------------------------------#
 # Parameters
 
-obs_pt = [point(0.,0.15,0.5), point(0.,-0.15,0.5) ]
+# obs_pt = [point(0.,0.15,0.5), point(0.,-0.15,0.5) ]
+# obs_pt = [point(0.,0.15,0.2), point(0.,-0.15,0.3) ]
+# obs_pt = point(0.,0.,0.5)
+# obs_pt = [point(0.,-0.02,0.5), point(0.,0.02,0.5), point(0.,-0.02,0.48), point(0.,0.02,0.48)]
+obs_pt = [point(0.,0.,0.5), point(0.,0.,0.25) ]
+
+
 side_dm = 0.2
 side_field_view = 0.5
 renew_scat = False
+nb_cells_side = 14
 nb_scat = 100
 
 #-----------------------------------------------------------------------------#
@@ -33,7 +40,7 @@ if os.path.exists(file_name) and not(renew_scat):
     dm.create_scat_from_csv(file_name)
 else:
     dm.generate_random_scatterers(nb_scat,
-      bounding_box = [-side_dm, side_dm, -side_dm, side_dm, 0.1, 0.2])
+      bounding_box = [-side_dm, side_dm, -side_dm, side_dm, 0.05, 0.1])
     dm.save_scat_pos(file_name)
 
 # fig, ax = dm.plot_scatterers()
@@ -47,12 +54,13 @@ else:
 # add a transmit array
 
 uc = simple_unit_cell()
-ta = transmit_array(10, 10, uc, horn)
+ta = transmit_array(nb_cells_side, nb_cells_side, uc, horn)
 # ta.plot(fig, ax)
 
 dm = desordered_medium(ta)
 dm.create_scat_from_csv(file_name)
-dm.set_polarizability(10.)
+dm.set_polarizability(0.001)
+# dm.source.set_random_phase_mask()
 
 field_1m_dm2 = dm.field(obs_pt)
 print("Field from horn + transmit array: " + str(np.abs(field_1m_dm2[0]).sum()))
@@ -64,7 +72,7 @@ dm.plot_field(plane="yz", side=side_field_view)
 #-----------------------------------------------------------------------------#
 # optimize a transmit array
 
-nb_repeat = 2
+nb_repeat = 1
 
 phase_states = dm.source.phase_mask
 nb_cells = len(phase_states)
@@ -79,9 +87,12 @@ for r in range(0, nb_repeat):
         field = dm.field(obs_pt)
         
         field_abs[r*nb_cells + idx] = np.abs(field[2]).sum()
-        print("iteration " + str(idx + 1) + ": " + str(field_abs[idx]) )
+        print("iteration " + str(idx + 1) + ": " 
+              + str(field_abs[r*nb_cells + idx]) )
         
-        if np.abs(field[2]).sum() < np.abs(field_obs[2]).sum():
+        # if np.abs(field[2]).sum() < np.abs(field_obs[2]).sum():
+        if (np.abs(field[2][0,0]) < np.abs(field_obs[2][0,0])) and \
+        (np.abs(field[2][0,1]) > np.abs(field_obs[2][0,1])):
             phase_states[idx] = 0
         else:
             field_obs = field
